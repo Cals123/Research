995c3cc9742ce4a62db7fb73d7283760bf9cb40b
Cals123 <calum.i.brown@btinternet.com>
2022-06-17 12:29:28 +0100

    Update Player.cs
    Basic jump implemented

--- My research project/Assets/Scripts/Player.cs
+++ My research project/Assets/Scripts/Player.cs
@@ -1,8 +1,8 @@
 /* 16/06/2022
  * NOTES: Initially tried to declare horizontal at class level - not able to use Input.GetAxis there, tried in start but this is not usable in update?
  * referred back to the course, declared the variable as locally as possible, used var as this is short and infers the type.
- * 
- * 
+ * 17/06/2022
+ * Want to build in jump system and work on improving the movement system (velocity dampening, desired velocity, max jumps, ground detection etc . . .
  * 
  * 
  * 
@@ -15,11 +15,9 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float _playerSpeed = 5;    // Declaring float value used in player speed calcs.
+    [SerializeField] float _jumpMagnitude = 5;
 
     Rigidbody2D _rigidbody2D;                   // Declaring the RigidBody2D with variable name - allows us to reference it at class level (anywhere in this script).
-
-
-
 
 
     void Start()
@@ -29,23 +27,27 @@ public class Player : MonoBehaviour
 
     }
 
-
     void Update()
     {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Jump button pressed");
+            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _jumpMagnitude);
+        }
+
+
         //float horizontal = Input.GetAxis("Horizontal");
         var horizontal = Input.GetAxis("Horizontal") * _playerSpeed;               // var allows the compiler to choose which type of value is used.
 
-
-
         if (horizontal < 0)
         {
-            _rigidbody2D.velocity = new Vector2(horizontal, _rigidbody2D.velocity.y);     // Sets the velocity of the object's rigidbody to a new 2d Vector.
-            Debug.Log($"Horizontal negative value detected. horizontal: {horizontal}");                                                    // Sends to console that negative horizontal motion detected.
+            _rigidbody2D.velocity = new Vector2(horizontal, _rigidbody2D.velocity.y);       // Sets the velocity of the object's rigidbody to a new 2d Vector.
+            Debug.Log($"Horizontal negative value detected. horizontal: {horizontal}");     // Sends to console that negative horizontal motion detected.
         }
         if (horizontal > 0)
         {
-            _rigidbody2D.velocity = new Vector2(horizontal, _rigidbody2D.velocity.y);
-            Debug.Log($"Horizontal negative value detected. horizontal: {horizontal},");
+            _rigidbody2D.velocity = new Vector2(horizontal, _rigidbody2D.velocity.y);       // Sets the velocity of the object's rigidbody to a new 2d Vector.
+            Debug.Log($"Horizontal positive value detected. horizontal: {horizontal},");    // Sends to console that positive horizontal motion detected.
         }
     }
 }
